35a050d2b261f190739b8bb6194bafa870ac6d7c
Otir <45825931+otirmesilati@users.noreply.github.com>
2022-03-11 17:58:20 +0200

    Integrated Lint + fixed problems reting to crlf

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send("<h1>server main page response :)</h1>");
+app.get('/', (req, res) => {
+  res.send('<h1>server main page response :)</h1>');
 });
 
 app.listen(port, () => {
-    console.log("up goes the server");
+  console.log('up goes the server');
 });
