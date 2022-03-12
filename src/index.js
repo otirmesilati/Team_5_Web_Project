f3e3c968c11307a1f1d3043c5b7b5e344e9f7249
Otir <45825931+otirmesilati@users.noreply.github.com>
2022-03-12 13:20:08 +0200

    swapping the text in the web page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>server main page response :)</h1>');
+  res.send('<h1>Welcome to team 8s web page!</h1>');
 });
 
 app.listen(port, () => {
