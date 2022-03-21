ed7388de7ba93c295e325117dcfb9847567005ff
Otir <45825931+otirmesilati@users.noreply.github.com>
2022-03-21 23:40:32 +0200

    Front update

---
+++ Front/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import App from "./App";
+
+
+import Homepage from './components/HomePage';
+import Navbar from './components/Navibar';
+import SignIn from './components/SignIn';
+
+
+ReactDOM.render(<App />, document.getElementById("root"))
